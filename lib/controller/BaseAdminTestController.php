0ac152f41817eba85b43e11c76a1d320706698bd
Sheldon <sheldon.els@gmail.com>
2011-04-18 10:30:12 +0100

    working test running, a button to run all tests at the top too

--- lib/controller/BaseAdminTestController.php
+++ lib/controller/BaseAdminTestController.php
@@ -4,12 +4,22 @@ class BaseAdminTestController extends AdminComponent{
   public $model_class = 'WildfireTest';
   public $display_name = "Tests";
   public $dashboard = false;
+  public $operation_actions = array('edit', 'run_test');
   
-  // public function index(){
-  //   foreach((array)param("selected_test") as $id) $this->test_results[$id] = Test::find($id)->run_test();
-  //   if($id = param("run_test")) $this->test_results[$id] = Test::find($id)->run_test();
-  //   $this->tests = Test::find("all");
-  // }
+  public function events(){
+    parent::events();
+    WaxEvent::add("cms.layout.sublinks", function(){
+      $obj = WaxEvent::data();
+      $obj->quick_links["Run All Tests"] = "/admin/tests/run_test/";
+    });
+  }
+  
+  public function run_test(){
+    WaxEvent::run("cms.form.setup", $this);
+    if($this->model->id) $this->model->run_test();
+    else foreach($this->model->all() as $row) $row->run_test();
+    $this->redirect_to("/admin/tests/");
+  }
   
   // public function edit_data(){
   //   if($class = param("model_class")){
