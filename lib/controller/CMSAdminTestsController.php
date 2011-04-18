aeefe818fafa4af6d555f5609f9b4ba6c75564b1
Sheldon <sheldon.els@gmail.com>
2011-04-18 14:00:59 +0100

    dont need this anymore

--- lib/controller/CMSAdminTestsController.php
+++ lib/controller/CMSAdminTestsController.php
@@ -21,22 +21,5 @@ class CMSAdminTestsController extends AdminComponent{
     $this->redirect_to("/admin/tests/");
   }
   
-  // public function edit_data(){
-  //   if($class = param("model_class")){
-  //     if(!$page = param("page")) $page = 1;
-  //     $this->model = new $class;
-  //     $this->recordset = $this->model->page($page, $this->per_page);
-  //   }else{
-  //     $this->classes = array();
-  //     //include all classes (besides the framework, to drop dependance on simpletest)
-  //     foreach(array_diff_key(Autoloader::$registry, array("framework"=>0)) as $role => $classes) foreach($classes as $class => $path) Autoloader::include_from_registry($class);
-  //     //find all models to list tests on them
-  //     foreach(get_declared_classes() as $class){
-  //       if(is_subclass_of($class, "WaxModel")){
-  //         $this->classes[] = $class;
-  //       }
-  //     }
-  //   }
-  // }
 }
 ?>
